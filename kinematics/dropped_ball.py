e1e2266e087c9567b0ef171b822a4e31e00f90e3
Nish_Neupane <manishneupane0909@gmail.com>
2026-01-15 18:22:56 -0600

    Update dropped_ball.py

--- kinematics/dropped_ball.py
+++ kinematics/dropped_ball.py
@@ -1,4 +1,6 @@
-# Problem 2.1: Dropped Ball
+# A ball is again dropped from a tower of height h with initial velocity zero. 
+# Write a program that asks the user to enter the height in meters of the tower and then calculates and prints the time the ball takes until it hits the ground, ignoring air resistance. 
+# Use your program to calculate the time for a ball dropped from a 100 m high tower
 import math
 
 g = 9.81
